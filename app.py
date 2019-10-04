d71a93a49a61698ed0ab95fc9c543e302b6048f0
natchancnc <56104964+natchancnc@users.noreply.github.com>
2019-10-04 09:06:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken'] 
     userText = decoded['queryResult']['intent']['displayName']
-    if (userTxet =='สวัสดี') :
+    if (userText =='สวัสดี') :
         sendText(user,'สวัสดีจ้ะ')
-    elif (userTxet =='สบายดีไหม') :
+    elif (userText =='สบายดีไหม') :
         sendText(user,'สบายดีจ้ะ')
     else :
         sendText(user,'อะไรอะ')
